64f80852d7e42c879fa45c52a3253574e67a6c83
triyono168 <triyono16893@gmail.com>
2019-08-19 23:18:49 +0700

    Add files via upload

---
+++ Tugas3.js
@@ -0,0 +1,40 @@
+var nama = 'Mikael'
+var peran = 'Penyihir'
+
+if (peran == '' && nama == ''){
+console.log("Nama harus diisi");
+}
+else if (peran == ''){
+  console.log("Halo ",nama,", Pilih peranmu untuk memulai game")
+}
+else {
+  console.log("Selamat datang di dunia Proxytia, ",nama)
+  if(peran =="Ksatria"){
+   console.log("Halo ",peran,nama,", kamu dapat menyerang dengan senjatamu") 
+  }
+  else if (peran == "Tabib"){
+  console.log("Halo ",peran,nama,", kamu akan membantu teman kamu yang terluka") 
+  }
+  else{
+    console.log("Halo ",peran,nama,", ciptakan keajaiban yang membantu kemenanganmu") 
+  }
+}
+
+
+/* Output untuk Input nama = '' dan peran = ''
+"Nama harus diisi!"
+
+//Output untuk Input nama = 'Mikael' dan peran = ''
+"Halo Mikael, Pilih peranmu untuk memulai game!"
+
+//Output untuk Input nama = 'Nina' dan peran 'Ksatria'
+"Selamat datang di Dunia Proxytia, Nina"
+"Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!"
+
+//Output untuk Input nama = 'Danu' dan peran 'Tabib'
+"Selamat datang di Dunia Proxytia, Danu"
+"Halo Tabib Danu, kamu akan membantu temanmu yang terluka."
+
+//Output untuk Input nama = 'Zero' dan peran 'Penyihir'
+"Selamat datang di Dunia Proxytia, Zero"
+"Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!"*/
